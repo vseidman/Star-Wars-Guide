5d79c565598890b7d7661672ea7f32996d6801cb
Victor Seidman <victorseidman@gmail.com>
2022-08-07 03:32:30 -0300

    Falta el carrusel

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -10,13 +10,6 @@ export const Navbar = () => {
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <Link className="nav-link" to="/planets">Planets</Link>
-        </li>
-      </ul>
-    </div>
   </div>
 </nav>
 	);
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -7,64 +7,40 @@ import Character from "../component/Character.jsx"
 import Planets from "../component/Planets.jsx";
 
 export const Home = () => {
-	const {store, actions} = useContext(Context);
+	const { store, actions } = useContext(Context);
 
 	return (
-	<>
-	<div className="container">
-		<div className="card-padre">
-			<div className="card-sw">
+		<>
 			<div className="container">
-        <div className="row">
-          {store.people && store.people.map((character,index) => {
-            return (
-              <Character
-                key={index}
-                character={character}
-              />
-            );
-          })}
-        </div>
-      </div>
-	  <div className="container">
-        <div className="row">
-          {store.planets && store.planets.map((planet,index) => {
-            return (
-              <Planets
-                key={index}
-                planetData={planet}
-              />
-            );
-          })}
-        </div>
-      </div>
+				<div className="card-padre">
+					<div className="card-sw">
+						<div className="container">
+							<div className="row"><h1 className="title-color">Characters</h1>
+								{store.people && store.people.map((character, index) => {
+									return (
+										<Character
+											key={index}
+											character={character}
+										/>
+									);
+								})}
+							</div>
+						</div>
+						<div className="container">
+							<div className="row"><h1 className="title-color">Planets</h1>
+								{store.planets && store.planets.map((planet, index) => {
+									return (
+										<Planets
+											key={index}
+											planetData={planet}
+										/>
+									);
+								})}
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
-		</div>
-	</div>
-      {/* <div className="container">
-        <div className="row">
-          {store.people && store.people.map((character,index) => {
-            return (
-              <Character
-                key={index}
-                character={character}
-              />
-            );
-          })}
-        </div>
-      </div>
-	  <div className="container">
-        <div className="row">
-          {store.planets && store.planets.map((planet,index) => {
-            return (
-              <Planets
-                key={index}
-                planetData={planet}
-              />
-            );
-          })}
-        </div>
-      </div> */}
-	</>
+		</>
 	);
 };
